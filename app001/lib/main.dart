866fb97f9c349ad3c49dcb5014ce319df0d67739
Tarcisio Silva <tarcisio.silva@restoque.com.br>
2023-05-13 15:56:10 -0300

    block orientation

--- app001/lib/main.dart
+++ app001/lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:app001/home/view/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Personal Expenses",
-      theme: ThemeData(
-        fontFamily: "Montserrat",
-        primaryColor: Colors.indigo,
-        appBarTheme: const AppBarTheme(
-          color: Colors.indigo,
-          centerTitle: true,
-        ),
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Personal Expenses",
+    theme: ThemeData(
+      fontFamily: "Montserrat",
+      primaryColor: Colors.indigo,
+      appBarTheme: const AppBarTheme(
+        color: Colors.indigo,
+        centerTitle: true,
       ),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      home: const HomePage(),
-    ));
+    ),
+    darkTheme: ThemeData.dark(),
+    themeMode: ThemeMode.system,
+    home: const HomePage(),
+  ));
+}
